6050366be809eb83feb7fffc03e0c35a0aad0ff0
shreyansh95763 <tshreyansh36@gmail.com>
2024-01-01 13:07:46 +0530

    background changes

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ const App = () => {
   const [position, setPosition] = useState('TOP'); // default position set to 'TOP'
 
   return (
+
     <div className="container">
       <h1>Tooltip Example</h1>
       <div className="controls">
+          {/* All buttons for different oprations to click */}
         <button onClick={() => setPosition('TOP')}>Top</button>
         <button onClick={() => setPosition('BOTTOM')}>Bottom</button>
         <button onClick={() => setPosition('LEFT')}>Left</button>
         <button onClick={() => setPosition('RIGHT')}>Right</button>
         </div>
 
-      <Tooltip position={position} text="This is a tooltip!">
+          {/* Tooltips container */}
+      <Tooltip className="tips" position={position} text="This is a tooltip!">
         <p>Hover over me to see tooltip</p>
       </Tooltip>
     </div>
--- src/Tooltip.js
+++ src/Tooltip.js
@@ -4,10 +4,12 @@ import './Tooltip.css'; // You can define your tooltip styles in Tooltip.css
 const Tooltip = ({ position, text, children }) => {
   const [showTooltip, setShowTooltip] = useState(false);
 
+      // on mouse hover 
   const handleMouseEnter = () => {
     setShowTooltip(true);
   };
 
+      // on mouse leave
   const handleMouseLeave = () => {
     setShowTooltip(false);
   };
